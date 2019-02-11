641a1f03a210e9e7364f1cf898b395ebb2195cf9
pravash <pravashbehera2@gmail.com>
2019-02-11 12:29:40 +0530

    Updates

---
+++ precache-manifest.721e5f1fa8c8417eeb8aa8701af7765a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dcc88ea5eb812b05549",
+    "url": "/event2/static/css/main.90d2ec26.chunk.css"
+  },
+  {
+    "revision": "3dcc88ea5eb812b05549",
+    "url": "/event2/static/js/main.3dcc88ea.chunk.js"
+  },
+  {
+    "revision": "9b6712dfb45092934271",
+    "url": "/event2/static/js/1.9b6712df.chunk.js"
+  },
+  {
+    "revision": "54b3b12c06260632951f",
+    "url": "/event2/static/js/runtime~main.54b3b12c.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/event2/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/event2/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/event2/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "8a39eaf812b83709a3d3d46ed043c7f1",
+    "url": "/event2/index.html"
+  }
+];
